e096cb7316d08319097f296dc458878fb64c3753
housseinisma <smithbiz100@gmail.com>
2025-08-30 04:31:23 +0300

    Fix authController import path in scheduleRoutes.js

--- server/routes/scheduleRoutes.js
+++ server/routes/scheduleRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const scheduleController = require('../controllers/scheduleController');
-const { protect, restrictTo } = require('../../controllers/authController');
+const { protect, restrictTo } = require('../controllers/authController');
 
 const router = express.Router();
 
