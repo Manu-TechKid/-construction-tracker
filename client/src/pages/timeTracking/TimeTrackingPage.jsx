420f0ab87a293e6ef254bc759f294201ae7aa0a5
housseinisma <smithbiz100@gmail.com>
2025-08-31 23:02:46 +0300

    fix: update AuthContext imports to use useAuth hook

--- client/src/pages/timeTracking/TimeTrackingPage.jsx
+++ client/src/pages/timeTracking/TimeTrackingPage.jsx
@@ -33,7 +33,7 @@ import {
 import { format, formatDistanceToNow, parseISO } from 'date-fns';
 import { useGetWorkerStatusQuery } from '../../features/workers/workersApiSlice';
 import { useCheckInWorkerMutation, useCheckOutWorkerMutation } from '../../features/timeTracking/timeTrackingApiSlice';
-import LocationHistoryMap from '../../features/timeTracking/LocationHistoryMap';
+import LocationHistoryMap from './LocationHistoryMap';
 import { useSnackbar } from 'notistack';
 import { useAuth } from '../../hooks/useAuth';
 
