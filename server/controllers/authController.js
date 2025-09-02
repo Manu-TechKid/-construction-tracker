c7eeac8fa7ebbdabb0c8f7ae78638ec350f8833c
housseinisma <smithbiz100@gmail.com>
2025-09-02 14:33:27 +0300

    Fix: JWT environment variable compatibility for authentication

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -8,7 +8,7 @@ const sendEmail = require('../utils/email');
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
+    expiresIn: process.env.JWT_EXPIRE || process.env.JWT_EXPIRES_IN || '30d',
   });
 };
 
@@ -16,7 +16,7 @@ const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
   const cookieOptions = {
     expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+      Date.now() + (process.env.JWT_COOKIE_EXPIRE || process.env.JWT_COOKIE_EXPIRES_IN || 30) * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
   };
