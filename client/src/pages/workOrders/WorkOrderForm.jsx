2af2a05bc82cab47cd2eebe397ef34cbcd2d2d68
housseinisma <smithbiz100@gmail.com>
2025-09-11 08:54:01 +0300

    fix(client): add missing useState import in WorkOrderForm

--- client/src/pages/workOrders/WorkOrderForm.jsx
+++ client/src/pages/workOrders/WorkOrderForm.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
