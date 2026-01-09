29b5202ff84851f073cdbcb1352d29775e69120b
housseinisma <smithbiz100@gmail.com>
2026-01-09 22:00:30 +0300

    fix(server): resolve server crash by removing handlerFactory dependency

--- server/controllers/workContactController.js
+++ server/controllers/workContactController.js
@@ -1,10 +1,56 @@
 const WorkContact = require('../models/workContactModel');
 const catchAsync = require('../utils/catchAsync');
-const factory = require('./handlerFactory');
+const AppError = require('../utils/appError');
 
-// Use the factory for standard CRUD operations
-exports.getAllWorkContacts = factory.getAll(WorkContact);
-exports.getWorkContact = factory.getOne(WorkContact);
-exports.createWorkContact = factory.createOne(WorkContact);
-exports.updateWorkContact = factory.updateOne(WorkContact);
-exports.deleteWorkContact = factory.deleteOne(WorkContact);
+exports.getAllWorkContacts = catchAsync(async (req, res, next) => {
+  const contacts = await WorkContact.find();
+  res.status(200).json({
+    status: 'success',
+    results: contacts.length,
+    data: contacts,
+  });
+});
+
+exports.getWorkContact = catchAsync(async (req, res, next) => {
+  const contact = await WorkContact.findById(req.params.id);
+  if (!contact) {
+    return next(new AppError('No work contact found with that ID', 404));
+  }
+  res.status(200).json({
+    status: 'success',
+    data: contact,
+  });
+});
+
+exports.createWorkContact = catchAsync(async (req, res, next) => {
+  const newContact = await WorkContact.create(req.body);
+  res.status(201).json({
+    status: 'success',
+    data: newContact,
+  });
+});
+
+exports.updateWorkContact = catchAsync(async (req, res, next) => {
+  const contact = await WorkContact.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!contact) {
+    return next(new AppError('No work contact found with that ID', 404));
+  }
+  res.status(200).json({
+    status: 'success',
+    data: contact,
+  });
+});
+
+exports.deleteWorkContact = catchAsync(async (req, res, next) => {
+  const contact = await WorkContact.findByIdAndUpdate(req.params.id, { deleted: true });
+  if (!contact) {
+    return next(new AppError('No work contact found with that ID', 404));
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
