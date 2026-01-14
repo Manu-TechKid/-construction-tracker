f6140fcd3a8b97fbfb17f819ad2be9db3f8c9ea0
housseinisma <smithbiz100@gmail.com>
2026-01-14 21:52:43 +0300

    feat(server): add detailed error logging to catchAsync

--- server/utils/catchAsync.js
+++ server/utils/catchAsync.js
@@ -1,5 +1,11 @@
 module.exports = fn => {
-    return (req, res, next) => {
-        fn(req, res, next).catch(next);
-    };
+  return (req, res, next) => {
+    fn(req, res, next).catch(err => {
+      console.error('--- ASYNC ERROR ---');
+      console.error('Error:', err);
+      console.error('Error Message:', err.message);
+      console.error('Stack Trace:', err.stack);
+      next(err);
+    });
+  };
 };
